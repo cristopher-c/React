b66bfda39cf4de1b547c48a58d5f181f571b8409
= <=>
2026-06-26 14:25:01 -0500

    Estilos a componentes & Impresión de productos con .map (Array)

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { FirsSteps } from './FirstSteps'
-import { MyAwesomeApp } from './MyAwesomeApp'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <FirsSteps/>
-    <MyAwesomeApp/>
   </StrictMode>
 )
