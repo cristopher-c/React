b0ee4f2327df93f24308165cf0557e40a5979d01
cristopher-c <cristosalamanca09@gmail.com>
2026-07-08 08:23:06 -0500

    Testing ejecutando eventos y validando valores

--- src/shoping-cart/ItemCounter.test.tsx
+++ src/shoping-cart/ItemCounter.test.tsx
@@ -1,6 +1,6 @@
 import { describe, expect, test } from "vitest";
 import ItemCounter from "./ItemCounter";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 describe('ItemCounter', () => {
   test('should render with default values', () => {
@@ -12,7 +12,41 @@ describe('ItemCounter', () => {
   test('should render with quantity defined', () => {
     const name = 'Test Item';
     const quantity = 25;
+
     render(<ItemCounter productName={name} productQuantity={quantity}/>);
     expect(screen.findByText(quantity)).toBeDefined();
+  })
+  
+  test('should decrease when -1 bottom is pressed', () => {
+    render(<ItemCounter productName={'Test Item'} productQuantity={10}/>);
+    
+    const [bottonToEvalute] = screen.getAllByRole('button');
+
+    fireEvent.click(bottonToEvalute);
+
+    expect(screen.findByText(9)).toBeDefined();
+    
+  })
+
+  test('should increase when +1 bottom is pressed', () => {
+    render(<ItemCounter productName={'Test Item'} productQuantity={10}/>);
+    
+    const [_,bottonToEvalute] = screen.getAllByRole('button');
+
+    fireEvent.click(bottonToEvalute);
+
+    expect(screen.findByText(11)).toBeDefined();
+    
+  })
+  
+  test('should not decrease when -1 bottom is pressed and quantity is 1', () => {
+    render(<ItemCounter productName={'Test Item'} productQuantity={1}/>);
+    
+    const [bottonToEvalute] = screen.getAllByRole('button');
+
+    fireEvent.click(bottonToEvalute);
+
+    expect(screen.findByText(1)).toBeDefined();
+    
   })
 })
